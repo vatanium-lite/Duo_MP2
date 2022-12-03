96f346270a51c5f48f17e8e1c6a7ee5f7b95a858
vatanium-lite <vatanak.cambodge@gmail.com>
2022-12-03 01:44:44 -0500

    Replace old files

--- Vata/test.py
+++ Vata/test.py
@@ -1,16 +1,28 @@
-import gbfs
+from gbfs import get_possible_states
 import matplotlib.pyplot as plt
+from kevinRushHours import Car
+from kevinRushHours import get_all_cars_in_grid
+from kevinRushHours import find_available_spaces_for_car_on_grid
+from kevinRushHours import move_car
+from kevinRushHours import get_car
+from kevinRushHours import create_grid
+from kevinRushHours import grid_to_string
+from gbfs import Node
+from gbfs import h1
+from gbfs import gbfs
+import queue
 
-# list = gbfs.string_splitter('BBIJ....IJCC..IAAMGDDK.MGH.KL.GHFFL.')
+file = 'Sample/sample-input.txt'
+input = open(file)
+puzzles = [line.strip() for line in [line for line in input.read().splitlines() if len(line) != 0] if line[0] != '#']
 
-grid = gbfs.grid_generator('BBB..MCCDD.MAAKL.MJ.KLEEJ.GG..JHHHII')
+node = Node(create_grid(puzzles[1]), h1)
 
-# n = gbfs.h1(grid)
-print(grid)
-# print(n)
-#
-# h3 = gbfs.h3(grid, 1.1)
-# print(h3)
+search = gbfs(node, h1)
+print(puzzles[1])
 
-# plt.imshow(grid)
-# plt.show()
+for s in search:
+    print(s)
+
+
+
